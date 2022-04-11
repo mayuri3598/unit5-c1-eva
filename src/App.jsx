e1fc3a61361b41824b59020346b0dbf48e658735
mayuri3598 <97494267+mayuri3598@users.noreply.github.com>
2022-04-11 13:10:05 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+mport { useState } from "react";
+
+import "./App.css";
 
 function App() {
   const [score, setScore] = useState(0);
-  const [wicket, setWicket] = useState(0);
-  const [over, setOver] = useState(0);
 
-  const addWicket = (value) => {
-    setWicket(wicket + value)
+  const [wicket, setWicket] = useState(0)
+  
+  const [over, setOver] = useState(0.0);
+
+  if(score>100){
+    var ans="INDIA WIN"
+  }
+  if(wicket==10){
+    var ans="INDIA LOSS"
   }
 
-  const addScore = (value) => {
-    setScore(score + value);
+
+  const handleChange = (val) => {
+    if (val === 0) {
+      setScore(0);
+    } 
+    else if (val === 4) {
+      setScore(score + 4);
+    } 
+    else if (val === 6) {
+      setScore(score + 6);
+    } 
+    else {
+      if (score + val < 0) return;
+      setScore(score + val);
+    }
+  };
+
+  const wickets=(wic)=>{
+    if (wic === 0) {
+      setWicket(0);
+    } 
+    else {
+      if (wicket + wic < 0) return;
+      setWicket(wicket + wic);
+    }
   }
-  const addBall = (value) =>{
-    setOver(over + value);
+
+
+  const overs=(ov)=>{
+    if (ov === 0) {
+      setOver(0);
+    } 
+    else {
+      if (over + ov < 0) return;
+      setOver(over + ov);
+    }
   }
+
   return (
     <div className="App">
       <h3>India:</h3>
@@ -26,14 +63,16 @@ function App() {
           <h1 className="scoreCount">
             {
               score
+              // show "score" here
             }
           </h1>
         </div>
         <div>
-          Wicket:{""}
+          Wicket:{" "}
           <h1 className="wicketCount">
             {
               wicket
+              // show wicket here
             }
           </h1>
         </div>
@@ -53,36 +92,37 @@ function App() {
 
       <div className="addScore">
         Add Score
-        {/* these buttons should add the respective amount in the score */}
-        <button className="addScore1"  onClick={() => {
-          addScore(1);
+        { score/* these buttons should add the respective amount in the score */}
+        <button className="addScore1" onClick={()=>{
+                 handleChange(1);
         }}>Add 1</button>
-        <button className="addScore4"  onClick={() => {
-          addScore(4);
+        <button className="addScore4" onClick={()=>{
+                    handleChange(4);
+
         }}>Add 4</button>
-        <button className="addScore6"  onClick={() => {
-          addScore(6);
+        <button className="addScore6" onClick={()=>{
+                    handleChange(6);
+
         }}>Add 6</button>
       </div>
 
       <div className="addWicket">
         Add Wicket
-        {/* Increase the count of wicket */}
-        <button onClick={() => {
-          addWicket(1);
+        { wicket/* Increase the count of wicket */}
+        <button onClick={()=>{
+          wickets(1)
         }}>Add 1 wicket</button>
       </div>
 
       <div className="addBall">
         Add ball
-        {/* Increase the total number of balls thrown here. */}
-        <button onClick={() =>{
-          addBall(1)
+        { over/* Increase the total number of balls thrown here. */}
+        <button onClick={()=>{
+          overs(1.0)
         }}>Add 1</button>
       </div>
 
-      { 
-      /* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+      {ans/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
     </div>
   );
 }
